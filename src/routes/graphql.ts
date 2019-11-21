b28b24c75fd20ea427734264cac534c0c6a03daf
Felix Becker <felix.b@outlook.com>
2019-11-21 18:10:53 +0100

    Use dynamic schema

--- src/routes/graphql.ts
+++ src/routes/graphql.ts
@@ -1,11 +1,23 @@
-import { buildSchema, formatError } from 'graphql'
+import {
+    buildSchema,
+    formatError,
+    GraphQLObjectType,
+    GraphQLNonNull,
+    GraphQLString,
+    GraphQLList,
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLFieldConfigArgumentMap,
+    GraphQLInt,
+    GraphQLInputObjectType,
+} from 'graphql'
 import { readFileSync } from 'fs'
 import graphQLHTTPServer from 'express-graphql'
 import * as pg from 'pg'
-import { listRepositories, validateRepository, validateCommit } from '../git'
+import { listRepositories, validateRepository, validateCommit, Signature, Commit } from '../git'
 import sql from 'sql-template-strings'
 import { Loaders, createLoaders } from '../loaders'
-import { Location, CodeSmell, UUID, RepoSpec, CommitSpec, FileSpec, Range } from '../models'
+import { Location, CodeSmell, UUID, RepoSpec, CommitSpec, FileSpec, Range, File } from '../models'
 import { transaction } from '../util'
 import { Duration, ZonedDateTime } from '@js-joda/core'
 import * as chardet from 'chardet'
@@ -17,82 +29,271 @@ interface Context {
     loaders: Loaders
 }
 
-// const SignatureType = new GraphQLObjectType<Signature>({
-//     name: 'Signature',
-//     fields: {
-//         name: { type: GraphQLNonNull(GraphQLString) },
-//         email: { type: GraphQLNonNull(GraphQLString) },
-//         date: { type: GraphQLNonNull(GraphQLString) },
-//     },
-// })
-
-// const FileType = new GraphQLObjectType<File>({
-//     name: 'File',
-//     fields: {
-//         path: { type: GraphQLNonNull(GraphQLString) },
-//         content: {
-//             type: GraphQLString,
-//             description: 'The file content from the git repository. null if the repository was not uploaded.',
-//             resolve: (
-//                 file: File & RepoSpec & CommitSpec,
-//                 args: {},
-//                 { loaders }: Context
-//             ): Promise<string | null> => loaders.fileContent.load(file),
-//         },
-//     },
-// })
-
-// const CommitType = new GraphQLObjectType<Commit>({
-//     name: 'Commit',
-//     fields: {
-//         sha: { type: GraphQLNonNull(GraphQLString) },
-//         message: { type: GraphQLNonNull(GraphQLString) },
-//         author: { type: GraphQLNonNull(SignatureType) },
-//         committer: { type: GraphQLNonNull(SignatureType) },
-//         files: {
-//             type: GraphQLNonNull(FileType),
-//             resolve: ({ repository, sha }: Commit, args: {}, { loaders }: Context): Promise<File[]> => {
-//                 return loaders.files.load({ repository, commit: sha })
-//             },
-//         },
-//     },
-// })
-
-// const CodeSmellLifeSpanType = new GraphQLObjectType({
-//     name: 'CodeSmellLifeSpanType',
-//     fields: {
-//         kind: { type: GraphQLString },
-//         instances: {
-//             type: new GraphQLList(CodeSmellType),
-//         },
-//     },
-// })
-
-// const RepositoryType = new GraphQLObjectType({
-//     name: 'Repository',
-//     fields: {
-//         name: { type: GraphQLString },
-//         commit: {
-//             type: CommitType,
-//             resolve: (source: RepoSpec, { sha }: { sha: string }, { loaders }: Context) => {
-//                 return loaders.commit.load({ ...source, commit: sha })
-//             },
-//         },
-//     },
-// })
-
-// const dynamicSchema = new GraphQLSchema({
-//     query: new GraphQLObjectType({
-//         name: 'Query',
-//         fields: {
-//             repositories: {
-//                 type: GraphQLNonNull(GraphQLList(RepositoryType)),
-//             },
-//         },
-//     }),
-// })
-
 export function createGraphQLHandler({ db, repoRoot }: { db: pg.Client; repoRoot: string }) {
+    var encodingArg: GraphQLFieldConfigArgumentMap = {
+        encoding: {
+            type: GraphQLString,
+            description: 'Encoding to use. If not given, will try to auto-detect, otherwise default to UTF8.',
+        },
+    }
+
+    var SignatureType = new GraphQLObjectType<Signature>({
+        name: 'Signature',
+        fields: {
+            name: { type: GraphQLNonNull(GraphQLString) },
+            email: { type: GraphQLNonNull(GraphQLString) },
+            date: { type: GraphQLNonNull(GraphQLString) },
+        },
+    })
+
+    var CommitType: GraphQLObjectType = new GraphQLObjectType<Commit>({
+        name: 'Commit',
+        fields: () => ({
+            sha: { type: GraphQLNonNull(GraphQLString) },
+            message: { type: GraphQLNonNull(GraphQLString) },
+            author: { type: GraphQLNonNull(SignatureType) },
+            committer: { type: GraphQLNonNull(SignatureType) },
+            parents: { type: GraphQLNonNull(GraphQLList(GraphQLNonNull(CommitType))) },
+            files: {
+                type: GraphQLNonNull(GraphQLList(GraphQLNonNull(FileType))),
+                description: 'The files that existed at this commit in the repository',
+                // resolve: ({ repository, sha }: Commit, args: {}, { loaders }: Context): Promise<File[]> => {
+                //     return loaders.files.load({ repository, commit: sha })
+                // },
+            },
+            codeSmells: {
+                type: GraphQLNonNull(GraphQLList(GraphQLNonNull(CodeSmellType))),
+            },
+        }),
+    })
+
+    var FileType = new GraphQLObjectType<File>({
+        name: 'File',
+        fields: {
+            path: { type: GraphQLNonNull(GraphQLString) },
+            content: {
+                type: GraphQLString,
+                args: encodingArg,
+                description:
+                    'The file content from the git repository. null if the repository was not uploaded.',
+            },
+            linesCount: { type: GraphQLInt },
+            commit: { type: GraphQLNonNull(CommitType) },
+        },
+    })
+
+    var positionFields = {
+        line: {
+            type: GraphQLNonNull(GraphQLInt),
+            description: 'The 0-based line number of the position',
+        },
+        character: {
+            type: GraphQLNonNull(GraphQLInt),
+            description: 'The 0-based character number of the position',
+        },
+    }
+    var PositionType = new GraphQLObjectType({
+        name: 'Position',
+        fields: positionFields,
+    })
+    var PositionInputType = new GraphQLInputObjectType({
+        name: 'PositionInput',
+        fields: positionFields,
+    })
+
+    var rangeFields = <P extends GraphQLObjectType | GraphQLInputObjectType>(positionType: P) => ({
+        start: {
+            type: GraphQLNonNull(positionType),
+            description: 'The start position of the range, inclusive.',
+        },
+        end: {
+            type: GraphQLNonNull(positionType),
+            description: 'The end position of the range, exclusive.',
+        },
+    })
+
+    var RangeType = new GraphQLObjectType({
+        name: 'Range',
+        fields: rangeFields(PositionType),
+    })
+    var RangeInputType = new GraphQLInputObjectType({
+        name: 'RangeInput',
+        fields: rangeFields(PositionInputType),
+    })
+
+    var LocationType = new GraphQLObjectType({
+        name: 'Location',
+        fields: {
+            file: {
+                type: GraphQLNonNull(FileType),
+            },
+            range: {
+                type: RangeType,
+            },
+            content: {
+                type: GraphQLString,
+                args: encodingArg,
+                description: 'The content of the range.',
+            },
+        },
+    })
+    var LocationInputType = new GraphQLInputObjectType({
+        name: 'LocationInput',
+        fields: {
+            file: {
+                type: GraphQLNonNull(GraphQLString),
+                description: 'The file path of the location.',
+            },
+            range: {
+                type: RangeInputType,
+            },
+        },
+    })
+
+    var CodeSmellType: GraphQLObjectType = new GraphQLObjectType({
+        name: 'CodeSmell',
+        fields: () => ({
+            id: {
+                type: GraphQLNonNull(GraphQLID),
+            },
+            locations: {
+                type: GraphQLList(GraphQLNonNull(LocationType)),
+            },
+            lifeSpan: {
+                type: GraphQLNonNull(CodeSmellLifeSpanType),
+                description: 'The complete lifespan of this code smell throughout commit history.',
+            },
+            predecessor: {
+                type: CodeSmellType,
+                description:
+                    'This code smell in a previous commit. This may not be in the direct parent commit because not every commit must be analyzed, but it is guaranteed to be in an ascendant commit.',
+            },
+            successor: {
+                type: CodeSmellType,
+                description:
+                    'This code smell in a later commit. This may not be in the direct child commit because not every commit must be analyzed, but it is guaranteed to be in a descendant commit.',
+            },
+            commit: {
+                type: CommitType,
+                description: 'The commit this code smell was detected in.',
+            },
+        }),
+    })
+
+    var CodeSmellLifeSpanType = new GraphQLObjectType({
+        name: 'CodeSmellLifeSpan',
+        description: 'A lifespan of a code smell throughout commit history.',
+        fields: {
+            kind: { type: GraphQLString },
+            instances: {
+                type: new GraphQLList(CodeSmellType),
+                description: 'The instances of the code smell throughout commit history.',
+            },
+            duration: {
+                type: GraphQLNonNull(GraphQLString),
+                description:
+                    'The duration this code smell was present in the codebase as an ISO8601 duration string',
+            },
+            interval: {
+                type: GraphQLNonNull(GraphQLString),
+                description:
+                    'The interval this code smell was present in the codebase as an ISO8601 interval string with start/end',
+            },
+        },
+    })
+
+    var RepositoryType = new GraphQLObjectType({
+        name: 'Repository',
+        fields: {
+            name: { type: GraphQLString },
+            commit: {
+                type: CommitType,
+                // resolve: (source: RepoSpec, { sha }: { sha: string }, { loaders }: Context) => {
+                //     return loaders.commit.load({ ...source, commit: sha })
+                // },
+            },
+            codeSmellLifespans: {
+                type: GraphQLNonNull(GraphQLList(GraphQLNonNull(CodeSmellLifeSpanType))),
+            },
+        },
+    })
+
+    var CodeSmellInputType = new GraphQLInputObjectType({
+        name: 'CodeSmellInput',
+        fields: {
+            id: {
+                type: GraphQLNonNull(GraphQLID),
+                description:
+                    'A client-provided globally unique ID (UUID). This is used to declare predecessors. If a code smell with this ID already exists, the code smell will be updated.',
+            },
+            kind: {
+                type: GraphQLNonNull(GraphQLString),
+                description:
+                    'An arbitrary string that uniquely identifies the kind of code smell, e.g. "GodClass". Must be the same for every instance.',
+            },
+            message: {
+                type: GraphQLString,
+                description:
+                    'A message for the code smell, which can be specific to this particular instance.',
+            },
+            predecessor: {
+                type: GraphQLString,
+                description:
+                    'Optional ID of a code smell in a previous commit, to define the life span of this code smell through the commit history.This will set up the successor for the referenced code smell as well.',
+            },
+            successor: {
+                type: GraphQLString,
+                description:
+                    'Optional ID of a code smell in a previous commit, to define the life span of this code smell through commits. This will set up the predecessor for the referenced code smell as well.',
+            },
+            locations: {
+                type: GraphQLList(GraphQLNonNull(LocationInputType)),
+                description: 'Locations of the code smell in the code.',
+            },
+        },
+    })
+
+    const dynamicSchema = new GraphQLSchema({
+        query: new GraphQLObjectType({
+            name: 'Query',
+            fields: {
+                codeSmell: {
+                    type: CodeSmellType,
+                },
+                repository: {
+                    type: RepositoryType,
+                    args: {
+                        name: {
+                            type: GraphQLNonNull(GraphQLString),
+                        },
+                    },
+                },
+                repositories: {
+                    type: GraphQLNonNull(GraphQLList(RepositoryType)),
+                },
+            },
+        }),
+        mutation: new GraphQLObjectType({
+            name: 'Mutation',
+            fields: {
+                addCodeSmells: {
+                    args: {
+                        repository: {
+                            type: GraphQLNonNull(GraphQLString),
+                        },
+                        commit: {
+                            type: GraphQLNonNull(GraphQLString),
+                        },
+                        codeSmells: {
+                            type: GraphQLNonNull(GraphQLList(CodeSmellInputType)),
+                        },
+                    },
+                    type: GraphQLNonNull(GraphQLList(GraphQLNonNull(CodeSmellType))),
+                },
+            },
+        }),
+    })
+
     class RepositoryResolver {
         constructor(public name: string) {}
 
@@ -216,11 +417,11 @@ export function createGraphQLHandler({ db, repoRoot }: { db: pg.Client; repoRoot
     }
 
     class FileResolver {
-        public get path(): string {
+        constructor(private spec: FileSpec & RepoSpec & CommitSpec) {}
+
+        path(): string {
             return this.spec.file
         }
-
-        constructor(private spec: FileSpec & RepoSpec & CommitSpec) {}
 
         async content({ encoding }: { encoding: string }, { loaders }: Context): Promise<string> {
             const content = await loaders.fileContent.load(this.spec)
@@ -311,7 +512,7 @@ export function createGraphQLHandler({ db, repoRoot }: { db: pg.Client; repoRoot
             loaders: createLoaders({ db, repoRoot }),
         }
         return {
-            schema,
+            schema: dynamicSchema,
             rootValue,
             graphiql: true,
             context,
