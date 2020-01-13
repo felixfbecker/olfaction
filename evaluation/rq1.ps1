e23c835c335a593911020bfda895d82c58a8adf6
Felix Becker <felix.b@outlook.com>
2020-01-13 01:20:10 +0100

    Fix RQ1 script

--- evaluation/rq1.ps1
+++ evaluation/rq1.ps1
@@ -1,18 +1,19 @@
 
 # 1. How long is the lifespan of specific kinds of code smells?
 
+[CmdletBinding()]
 param(
-    [string] $Kind
+    [Uri] $ServerUrl = [Uri]::new("http://localhost:4040")
 )
 
 $body = (@{
     query = '
-        {
+        query {
             repositories {
                 edges {
                     node {
                         # Get first and last commit date to calculate percentage of the total project age
-                        initialCommit: commits(last: 1) {
+                        commits {
                             edges {
                                 node {
                                     author {
@@ -21,18 +22,10 @@ $body = (@{
                                 }
                             }
                         }
-                        headCommit: commits(first: 1) {
+                        codeSmellLifespans {
                             edges {
                                 node {
-                                    author {
-                                        date
-                                    }
-                                }
-                            }
-                        }
-                        codeSmellLifespans(kind: $kind) {
-                            edges {
-                                node {
+                                    kind
                                     duration
                                 }
                             }
@@ -42,22 +35,31 @@ $body = (@{
             }
         }
     '
-    variables = @{
-        repository = $Repository
-        kind = $Kind
-    }
+    variables = @{}
 } | ConvertTo-Json)
+Write-Verbose "Body: $body"
 
-Invoke-RestMethod -Body $body |
-    ForEach-Object { $_.data.repositories.edges } |
+$result = Invoke-RestMethod -Method POST -Uri ([Uri]::new($ServerUrl, "/graphql")) -Body $body -ContentType 'application/json'
+if ($result.PSObject.Properties['errors'] -and $result.errors) {
+    throw ($result.errors | ConvertTo-Json -Depth 100)
+}
+$result.data.repositories.edges |
     ForEach-Object {
-        $repo = $_
-        $headCommitDate = $repo.headCommit.edges[0].node.author.date
-        $initialCommitDate = $repo.initialCommit.edges[0].node.author.date
+        $repo = $_.node
+        $headCommitDate = $repo.commits.edges[0].node.author.date
+        $initialCommitDate = $repo.commits.edges[$repo.commits.edges.Length - 1].node.author.date
         $projectAge = $headCommitDate - $initialCommitDate
 
-        $repo.codeSmellLifespans.edges |
-            ForEach-Object { [System.Xml.XmlConvert]::ToTimeSpan($_.node.duration) } |
-            ForEach-Object { $_ / $projectAge }
+        $repo.codeSmellLifespans.edges | ForEach-Object {
+            [pscustomobject]@{
+                Kind = $_.node.kind
+                Age = [System.Xml.XmlConvert]::ToTimeSpan($_.node.duration) / $projectAge
+            }
+        }
     } |
-    Measure-Object -AllStats
+    Group-Object -Property Kind |
+    ForEach-Object {
+        $measure = $_.Group | Measure-Object -AllStats -Property Age
+        Add-Member -MemberType NoteProperty -Name Kind -Value $_.Name -InputObject $measure
+        $measure
+    }
